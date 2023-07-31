0bb1659f4d2b2717e4260911ce97cdaf0beefa4d
Stephanie Guzman <sguzman247@gmail.com>
2023-07-31 15:37:41 -0500

    feat: add react-router to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import {
+  createBrowserRouter, RouterProvider,
+} from "react-router-dom"
+import App from './routes/App'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider
+      router={router}
+    />
   </React.StrictMode>,
 )
