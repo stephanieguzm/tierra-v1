45c0360cbc2a0e347b10cd45bb78d31a025a7be1
Stephanie Guzman <sguzman247@gmail.com>
2023-07-31 11:55:29 -0500

    chore: create components and connect together

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import ExercisePage from './ExercisePage'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl'>tierra</h1>
+      <ExercisePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ExerciseContainer.tsx
@@ -0,0 +1,13 @@
+import Exercise from "./Exercise"
+
+const ExerciseContainer = () => {
+
+  return (
+    <section>
+      <h3 className='text-xl'>container</h3>
+      <Exercise />
+    </section>
+  )
+}
+
+export default ExerciseContainer
